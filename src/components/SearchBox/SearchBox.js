63c98568dab613d021ed4bde7120b797ec3d96a8
robdll <dilillo.roberto@gmail.com>
2022-05-28 12:10:20 +0100

    update searchbox ui

--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -1,11 +1,35 @@
 import styles from './SearchBox.module.scss';
+import { useState } from 'react';
 
 function SearchBox() {
+
+  const PLACEHOLDER_DEFAULT = 'Search for a movie title';
+  const [searchValue, setSearchValue] = useState('');
+  const [placeholder, setPlaceholder] = useState(PLACEHOLDER_DEFAULT);
+  const [inputClasses, setInputClasses] = useState(`${styles.input}`);
+
+  const handleClick = (e) => {
+    setSearchValue(e.target.value)
+  }
+  
+  const handleFocus = (e) => {
+    setPlaceholder('')
+    setInputClasses(`${styles.input} ${styles.alignLeft}`);
+  }
+
+  const handleBlur = (e) => {
+    if(searchValue === '') {
+      setPlaceholder(PLACEHOLDER_DEFAULT)
+      setInputClasses(`${styles.input}`);
+    }
+  }
+
   return (
     <div className={styles.searchBox}>
-        search
+      <input onFocus={handleFocus} onBlur={handleBlur} className={inputClasses} value={searchValue} onChange={handleClick} placeholder={placeholder}></input>
     </div>
-  );
-}
-
-export default SearchBox;
+    );
+  }
+  
+  export default SearchBox;
+  
