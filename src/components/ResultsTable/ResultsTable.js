41fb70f5493364bb0dac5759b63fdde217319e37
robdll <dilillo.roberto@gmail.com>
2022-05-28 19:08:58 +0100

    adds result table

--- src/components/ResultsTable/ResultsTable.js
+++ src/components/ResultsTable/ResultsTable.js
@@ -1,10 +1,25 @@
 import styles from './ResultsTable.module.scss';
+import { useSelector } from 'react-redux';
 
 function ResultsTable() {
-  return (
-    <div className={styles.ResultsTable}>
-        results
+  const results = useSelector((state) => state.movieState.movies);
+  console.log(results)
+  const rows = results.map( item => {
+    const coverCell = <img className={styles.cover} src={item.poster_path} alt={`${item.title} cover`} />
+    const titleCell = <div className={styles.title}>{item.title}</div>
+    const watchCell = <div className={styles.action}>icon</div>
+    const faveCell = <div className={styles.action}>icon</div>
+    return <div className={styles.resultRow}> 
+      {coverCell}
+      {titleCell}
+      {watchCell}
+      {faveCell}
     </div>
+  })
+  return (
+    <article className={styles.resultsTable}>
+        {rows}
+    </article>
   );
 }
 
