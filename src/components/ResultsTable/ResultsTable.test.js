75344798bdaea2c275fd84791926820050bbc1b1
robdll <dilillo.roberto@gmail.com>
2022-05-28 23:34:50 +0100

    delete unneded tests

--- src/components/ResultsTable/ResultsTable.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import ResultsTable from './ResultsTable';
-
-test('renders learn react link', () => {
-  render(<ResultsTable />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
