f4f841840fd0cf3a0790e4e541573bb9bc25fba4
robdll <dilillo.roberto@gmail.com>
2022-05-29 10:26:02 +0100

    adds result table tests

--- src/tests/components.spec.js
+++ src/tests/components.spec.js
@@ -9,30 +9,15 @@ import { Provider } from 'react-redux'
 import { store } from '../store/store'
 
 const movie = {
-    adult: false,
     backdrop_path: '/6jz8ayHiRNt0s1wiSdxemLfzGAX.jpg',
-    genre_ids: [
-      16,
-      10751,
-      35
-    ],
     id: 15165,
-    original_language: 'en',
-    original_title: 'Barbie as The Princess & the Pauper',
-    overview: 'In her first animated musical featuring seven original songs, Barbie comes to life in this modern re-telling of a classic tale of mistaken identity and the power of friendship. Based on the story by Mark Twain,',
-    popularity: 85.652,
-    poster_path: 'https://image.tmdb.org/t/p/w92/xHYsUwUe4MaNc6mbNBGTkqZSnPk.jpg',
-    release_date: '2004-09-28',
     title: 'Barbie as The Princess & the Pauper',
-    video: false,
-    vote_average: 7.5,
-    vote_count: 1206
 }
 
 // We use msw to intercept the network request during the test,
 // and return few movie in the response after 150ms
 export const handlers = [
-    rest.get('/search/movie', (req, res, ctx) => {
+    rest.get('https://api.themoviedb.org/3/search/movie', (req, res, ctx) => {
         return res(ctx.json([
             movie, movie, movie
         ]), ctx.delay(150))
@@ -44,19 +29,8 @@ const server = setupServer(...handlers)
 // Enable API mocking before tests.
 beforeAll(() => server.listen())
 
-let container;
-
-// Reset any runtime request handlers we may add during the tests.
-beforeEach(() => {
-    container = document.createElement('Provider');
-    document.body.appendChild(container);
-})
-
-
 // Reset any runtime request handlers we may add during the tests.
 afterEach(() => {
-    document.body.removeChild(container);
-    container = null;
     return server.resetHandlers()
 });
 
@@ -79,24 +53,19 @@ describe('Components test', () => {
         render(<App />)
         const inputNode = screen.getByPlaceholderText('Search for a movie title')
         fireEvent.change(inputNode, {target: {value: 'as'}})
-        console.log(inputNode.value)
         expect(inputNode.value).toEqual('as')
     })
 
-    it('Should search for movies', async () => {
-        act(() => {
-            ReactDOM.createRoot(container).render(<Provider store={store}><App /></Provider>);
-        });
-
+    it('Should search for movies and populate display result table', async () => {
+        render(<Provider store={store}><App /></Provider>);
         const inputNode = screen.getByTestId('search-input')
         fireEvent.change(inputNode, {target: {value: 'as'}})
         const searchIcon = screen.getByTestId('search-icon')
-        act(()=> {
+        await act(()=> {
             fireEvent.click(searchIcon)
             const resultTable = screen.getByTestId('result-table')
             expect(resultTable).toBeInTheDocument();
-        })
-        
+        })    
     })
-      
+
 })
